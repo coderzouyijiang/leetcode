0cde0ad612909b15a27f78330dbb554bc1a0d083
zouyijiang <zouyijiang@163.com>
2019-12-29 19:04:01 +0800

    用bitSet代替int位运算，适应更大的范围

--- src/main/java/leetcode/editor/cn/P76_MinimumWindowSubstring.java
+++ src/main/java/leetcode/editor/cn/P76_MinimumWindowSubstring.java
@@ -15,6 +15,9 @@
 
 package leetcode.editor.cn;
 
+import java.util.Arrays;
+import java.util.BitSet;
+
 //Java：Minimum Window Substring
 public class P76_MinimumWindowSubstring {
     public static void main(String[] args) {
@@ -23,35 +26,44 @@ public class P76_MinimumWindowSubstring {
 //        输入: S = "ADOBECODEBANC", T = "ABC"
 //        输出: "BANC"
         System.out.println("minWindow:" + solution.minWindow("ADOBECODEBANC", "ABC"));
+//        System.out.println("minWindow:" + solution.minWindow("A", "A"));
+//        System.out.println("minWindow:" + solution.minWindow("a", "a"));
     }
 
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String minWindow(String s, String t) {
-            int[] chMasks = new int['Z' + 1]; // 字符->字符id
+            int[] chIds = new int[128]; // 字符->字符id
+            Arrays.fill(chIds, -1);
             int chNum = 0;
-            int target = 0;
+            BitSet target = new BitSet();
             for (int i = 0; i < t.length(); i++) {
                 char ch = t.charAt(i);
-                if (chMasks[ch] == 0) {
-                    target |= (chMasks[ch] = 1 << chNum++);
+                if (chIds[ch] == -1) {
+//                    target |= (chMasks[ch] = 1 << chNum++);
+                    chIds[ch] = chNum;
+                    target.set(chNum);
+                    chNum++;
                 }
             }
             if (s.length() < chNum) return "";
             String result = null;
             int m = s.length() - chNum;
-            for (int i = 0; i < m; i++) {
-                int state = 0;
+            for (int i = 0; i <= m; i++) {
+//                int state = 0;
+                BitSet state = new BitSet(chNum);
                 for (int j = i; j < s.length(); j++) {
-                    state |= chMasks[s.charAt(j)];
+//                    state |= chMasks[s.charAt(j)];
+                    int chId = chIds[s.charAt(j)];
+                    if (chId == -1) continue;
+                    state.set(chId);
                     System.out.printf("[%s,%s]=%s,state=%s,len=%s\n", i, j, s.substring(i, j + 1), state, j + 1 - i);
-                    if (state == target) {
-                        if (result == null || j + 1 - i < result.length()) {
-                            result = s.substring(i, j + 1);
-                        } else {
-                            break;
-                        }
+                    if (!state.equals(target)) continue;
+                    if (result == null || j + 1 - i < result.length()) {
+                        result = s.substring(i, j + 1);
+                    } else {
+                        break;
                     }
                 }
             }
